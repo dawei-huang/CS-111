e3cb73bb1d35a7ec4b42b3ac4a902ea38ce70ca7
Nathan Smith <nathan@nathansmith.io>
2017-11-21 13:22:24 -0800

    clear some errors

--- lab3a/lab3a.c
+++ lab3a/lab3a.c
@@ -182,9 +182,6 @@ void printGroups()
 
 int main(int argc, char** argv)
 {
-
-  char* file_system_name;
-
   if (argc != 2) {
     fprintf(stderr, "Please enter one and only one argument.\n");
     exit(1);
@@ -197,7 +194,7 @@ int main(int argc, char** argv)
   }
 
   printSuperblocks();
-  printGroups();
+  // printGroups();
   /*
   printFreeBlockEntries();
   printFreeInodeEntries();
